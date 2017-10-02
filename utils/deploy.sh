d0864a4a24a9ce7c054d844ae6654b7e21dedfa6
dslaveykov <dslaveykov@axway.com>
2017-10-02 10:24:18 +0200

    Changes for previous commit - Change the way we set the path to the project's settings.xml

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn --settings="../settings.xml" release:clean -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
-mvn --settings="../settings.xml" release:prepare -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false" 
-mvn --settings="../settings.xml" release:perform -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
+mvn -s ../settings.xml release:clean -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
+mvn -s ../settings.xml release:prepare -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false" 
+mvn -s ../settings.xml release:perform -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
