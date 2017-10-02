83266ba203ff186d7281772d7118792a246d38d3
Dragoslav Slaveykov <dslaveykov@axway.com>
2017-10-02 12:37:13 +0200

    Minor change in setting settings.xml path

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn -s ../settings.xml release:clean -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
-mvn -s ../settings.xml release:prepare -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false" 
-mvn -s ../settings.xml release:perform -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
+mvn -s ./settings.xml release:clean -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
+mvn -s ./settings.xml release:prepare -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false" 
+mvn -s ./settings.xml release:perform -DautoVersionSubmodules=true -Darguments="-DskipTests=true -Dgpg.skip=false"
